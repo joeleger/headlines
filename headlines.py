a63e65dca407589104f7debc183015d2876e6f25
joeleger <joeleger@users.noreply.github.com>
2020-05-11 18:29:06 -0400

    adding cookies and style basics

--- headlines.py
+++ headlines.py
@@ -1,8 +1,9 @@
+import datetime
 import os
 import json
 import urllib
 import feedparser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 OPEN_WEATHER_API_KEY = os.environ.get("OPEN_WEATHER")
@@ -12,8 +13,8 @@ CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=" + OPEN_C
 
 DEFAULTS = {'publication': 'bbc',
             'city': 'London,UK',
-            'currency_from':'CAD',
-            'currency_to':'USD'
+            'currency_from': 'CAD',
+            'currency_to': 'USD'
             }
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
@@ -23,41 +24,48 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              }
 
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
+
 @app.route('/')
 def home():
     # get customized headlines, based on user input or default
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    publication = get_value_with_fallback('publication')
+    pub_title = str.title(publication)
+
     articles = get_news(publication)
     # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback('city')
     weather = get_weather(city)
     # get customized currency based on user input or default
-    currency_from = request.args.get("currency_from")
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get("currency_to")
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
+
     rate, currencies = get_rate(currency_from, currency_to)
-    return render_template("home.html", articles=articles,
-                           weather=weather,
-                           currency_from=currency_from,
-                           currency_to=currency_to,
-                           rate=rate,
-                           currencies=sorted(currencies))
+    response = make_response(render_template("home.html",
+                                             pub_title=pub_title,
+                                             articles=articles,
+                                             weather=weather,
+                                             currency_from=currency_from,
+                                             currency_to=currency_to,
+                                             rate=rate,
+                                             currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=365)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from",
+                        currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
 
 
-def get_news(query):
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication.lower()])
     return feed['entries']
 
 
@@ -82,7 +90,6 @@ def get_rate(frm, to):
     frm_rate = parsed.get(frm.upper())
     to_rate = parsed.get(to.upper())
     return to_rate / frm_rate, parsed.keys()
-
 
 
 if __name__ == "__main__":
